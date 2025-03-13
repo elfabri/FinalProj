3547bd053e24fd2942e641d3a1bb64c65bc7bea9
elfabri <111920054+elfabri@users.noreply.github.com>
2025-03-12 22:51:04 -0300

    attack 1

--- Assets/Scripts/Input/InputManager.cs
+++ Assets/Scripts/Input/InputManager.cs
@@ -10,11 +10,16 @@ public class InputManager : MonoBehaviour
     public static bool JumpIsHeld;
     public static bool JumpWasReleased;
 
+    public static bool Attack_1_WasPressed;
+    public static bool Attack_1_IsHeld;
+    public static bool Attack_1_WasReleased;
+
     public static bool UiSubmitPressed;
     public static bool UiCancelPressed;
 
     private InputAction _moveAction;
     private InputAction _jumpAction;
+    private InputAction _attack_1_Action;
     private InputAction _uiSubmit;
     private InputAction _uiCancel;
 
@@ -24,6 +29,7 @@ public class InputManager : MonoBehaviour
 
         _moveAction = PlayerInput.actions["Move"];
         _jumpAction = PlayerInput.actions["Jump"];
+        _attack_1_Action = PlayerInput.actions["Attack1"];
 
         _uiSubmit = PlayerInput.actions["Submit"];
         _uiCancel = PlayerInput.actions["Cancel"];
@@ -36,6 +42,10 @@ public class InputManager : MonoBehaviour
         JumpWasPressed = _jumpAction.WasPressedThisFrame();
         JumpIsHeld = _jumpAction.IsPressed();
         JumpWasReleased = _jumpAction.WasReleasedThisFrame();
+
+        Attack_1_WasPressed = _attack_1_Action.WasPressedThisFrame();
+        Attack_1_IsHeld = _attack_1_Action.IsPressed();
+        Attack_1_WasReleased = _attack_1_Action.WasReleasedThisFrame();
 
         UiSubmitPressed = _uiSubmit.WasPressedThisFrame();
         UiCancelPressed = _uiCancel.WasPressedThisFrame();
