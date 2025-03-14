c4a3322ad7975d246c7625166ff5b67499a212ce
elfabri <111920054+elfabri@users.noreply.github.com>
2025-03-13 22:02:13 -0300

    attack 1, 2 and heavy animation control

--- Assets/Scripts/PlayerAnimations.cs
+++ Assets/Scripts/PlayerAnimations.cs
@@ -7,6 +7,9 @@ public class PlayerAnimations : ScriptableObject
     public static readonly int Jump = Animator.StringToHash("Jump");
     public static readonly int Fall = Animator.StringToHash("Fall");
     public static readonly int Run = Animator.StringToHash("Run");
+    public static readonly int PreAttack_1 = Animator.StringToHash("PreAttack");
     public static readonly int Attack_1 = Animator.StringToHash("Attack1");
+    public static readonly int PreAttack_2 = Animator.StringToHash("PreAttack2");
     public static readonly int Attack_2 = Animator.StringToHash("Attack2");
+    public static readonly int HeavyAttack = Animator.StringToHash("HeavyAttack");
 }
